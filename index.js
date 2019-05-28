ac904c26ec7ed09c5236cdafd38ba6e44381ff14
fangtao04 <fangtao04@meituan.com>
2019-05-28 20:32:21 +0800

    js-ejs

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const { relative, resolve } = require('path');
+const parseEslint = require('./lib/parse');
 const root = process.cwd();
+
+const formatEslintResults = (results = [], rulesMetaUrlMap) => {
+    results.forEach(v => {
+        const { filePath, messages } = v;
+        v.filePath = relative(root, filePath);
+        v.messages = messages.map(v2 => {
+            return {
+                ...v2,
+                ruleIdUrl: rulesMetaUrlMap[ruleId]
+            };
+        });
+        delete v.source;
+    });
+};
+
+const getRulesMetaUrlMap = (rulesMeta = {}) => {
+    return Object.entries(rulesMeta).reduce((prev, cur) => {
+        const [k, v] = cur;
+        prev[k] = v.docs.url;
+        return prev;
+    }, {});
+};
 
 module.exports = function(results, data) {
     const { rulesMeta = {} } = data;
+    const rulesMetaUrlMap = getRulesMetaUrlMap(rulesMeta);
 
-    results.forEach(v => {
-        v.filePath = relative(root, v.filePath);
-        delete v.source;
-    });
+    formatEslintResults(results, rulesMetaUrlMap);
 
-    return 'hello';
+    return parseEslint(results);
 };
