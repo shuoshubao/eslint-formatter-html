5ae8f2e50af2f196f503815944cdb4bfbf3c67bf
shuoshubao <759979885@qq.com>
2022-12-30 17:16:13 +0800

    feat: matchMedia, prefers-color-scheme

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   printWidth: 120,
-  useTabs: false,
-  tabWidth: 2,
   semi: false,
   singleQuote: true,
   trailingComma: 'none',
-  proseWrap: 'never',
-  arrowParens: 'avoid',
-  bracketSpacing: true,
-  htmlWhitespaceSensitivity: 'ignore'
+  arrowParens: 'avoid'
 }
